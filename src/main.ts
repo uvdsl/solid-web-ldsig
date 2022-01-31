b74762be0c61fefb549c0746fe398a2dc6f089c3
ik1533 <braun@kit.edu>
2022-01-31 19:08:53 +0100

    added service worker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import "./registerServiceWorker";
+import "./registerServiceWorker";
 import router from "./router";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
